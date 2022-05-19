fb11a09df08750151c55c336393be6c89329b221
ModyQyW <wurui-dev@foxmail.com>
2022-05-19 19:08:48 +0800

    chore: update deps and config

--- examples/react/vite.config.ts
+++ examples/react/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from '@modyqyw/vite-plugin-eslint';
+import inspect from 'vite-plugin-inspect';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [react(), eslint(), inspect()],
 });
--- examples/vue/vite.config.ts
+++ examples/vue/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import eslint from '@modyqyw/vite-plugin-eslint';
+import inspect from 'vite-plugin-inspect';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), eslint()],
+  plugins: [vue(), eslint(), inspect()],
 });
