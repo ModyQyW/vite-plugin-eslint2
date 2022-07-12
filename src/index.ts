41cb889bbb9404063c475c3c370054f72fa5841f
ModyQyW <wurui-dev@foxmail.com>
2022-07-12 13:17:29 +0800

    feat!: lint on start

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import type * as Vite from 'vite';
 import type * as ESLint from 'eslint';
 import * as path from 'path';
 
-export type FilterPattern = string | string[] | null;
+export type FilterPattern = string | string[];
 
 export interface VitePluginESLintOptions extends ESLint.ESLint.Options {
   cache?: boolean;
@@ -13,10 +13,7 @@ export interface VitePluginESLintOptions extends ESLint.ESLint.Options {
   exclude?: FilterPattern;
   eslintPath?: string;
   formatter?: string;
-  /** @deprecated Recommend to use `emitError` */
-  throwOnError?: boolean;
-  /** @deprecated Recommend to use `emitWarning` */
-  throwOnWarning?: boolean;
+  lintOnStart?: boolean;
   emitError?: boolean;
   emitErrorAsWarning?: boolean;
   emitWarning?: boolean;
@@ -40,9 +37,10 @@ export default function ESLintPlugin(options: VitePluginESLintOptions = {}): Vit
   const defaultFormatter = 'stylish';
   const formatter = options?.formatter ?? defaultFormatter;
   let loadedFormatter: ESLint.ESLint.Formatter;
-  const emitError = options?.emitError ?? options?.throwOnError ?? true;
+  const lintOnStart = options?.lintOnStart ?? true;
+  const emitError = options?.emitError ?? true;
   const emitErrorAsWarning = options?.emitErrorAsWarning ?? false;
-  const emitWarning = options?.emitWarning ?? options?.throwOnWarning ?? true;
+  const emitWarning = options?.emitWarning ?? true;
   const emitWarningAsError = options?.emitWarningAsError ?? false;
 
   const filter = createFilter(include, exclude);
@@ -50,31 +48,74 @@ export default function ESLintPlugin(options: VitePluginESLintOptions = {}): Vit
 
   let eslint: ESLint.ESLint;
   let outputFixes: typeof ESLint.ESLint.outputFixes;
+  let lintFiles: (files: FilterPattern) => Promise<void>;
 
   return {
     name: 'vite:eslint',
     async buildStart() {
       // initial
       if (!eslint || !loadedFormatter || !outputFixes) {
-        await import(eslintPath)
-          .then(async (module) => {
-            eslint = new module.ESLint({
-              ...options,
-              cache,
-              cacheLocation,
-            });
-            loadedFormatter = await eslint.loadFormatter(formatter);
-            outputFixes = module.ESLint.outputFixes.bind(module.ESLint);
-          })
-          .catch((error) => {
-            console.log('');
-            this.error(
-              `${
-                error?.message ??
-                'Failed to import ESLint. Have you installed and configured correctly?'
-              }`,
-            );
+        try {
+          const module = await import(eslintPath);
+          const eslint: ESLint.ESLint = new module.ESLint({
+            ...options,
+            cache,
+            cacheLocation,
           });
+          loadedFormatter = await eslint.loadFormatter(formatter);
+          outputFixes = module.ESLint.outputFixes.bind(module.ESLint);
+          lintFiles = async (files) =>
+            await eslint
+              .lintFiles(files)
+              .then(async (lintResults: ESLint.ESLint.LintResult[] | void) => {
+                if (!lintResults) return;
+
+                if (lintResults.length > 0 && options.fix) {
+                  outputFixes(lintResults);
+                }
+
+                if (lintResults.some((item) => item.errorCount > 0) && emitError) {
+                  const formatResult = await loadedFormatter.format(
+                    lintResults.filter((item) => item.errorCount > 0),
+                  );
+                  console.log('');
+                  if (emitErrorAsWarning) {
+                    this.warn(formatResult);
+                  } else {
+                    this.error(formatResult);
+                  }
+                }
+
+                if (lintResults.some((item) => item.warningCount > 0) && emitWarning) {
+                  const formatResult = await loadedFormatter.format(
+                    lintResults.filter((item) => item.warningCount > 0),
+                  );
+                  console.log('');
+                  if (emitWarningAsError) {
+                    this.error(formatResult);
+                  } else {
+                    this.warn(formatResult);
+                  }
+                }
+              })
+              .catch((error) => {
+                console.log('');
+                this.error(`${error?.message ?? error}`);
+              });
+        } catch (error) {
+          console.log('');
+          this.error(
+            `${
+              (error as Error)?.message ??
+              'Failed to import ESLint. Have you installed and configured correctly?'
+            }`,
+          );
+        }
+      }
+
+      // lint on start
+      if (lintOnStart) {
+        await lintFiles(include);
       }
     },
     async transform(_, id) {
@@ -101,43 +142,7 @@ export default function ESLintPlugin(options: VitePluginESLintOptions = {}): Vit
         return null;
       }
 
-      await eslint
-        .lintFiles(file)
-        .then(async (lintResults: ESLint.ESLint.LintResult[] | void) => {
-          if (!lintResults) return;
-
-          if (lintResults.length > 0 && options.fix) {
-            outputFixes(lintResults);
-          }
-
-          if (lintResults.some((item) => item.errorCount > 0) && emitError) {
-            const formatResult = await loadedFormatter.format(
-              lintResults.filter((item) => item.errorCount > 0),
-            );
-            console.log('');
-            if (emitErrorAsWarning) {
-              this.warn(formatResult);
-            } else {
-              this.error(formatResult);
-            }
-          }
-
-          if (lintResults.some((item) => item.warningCount > 0) && emitWarning) {
-            const formatResult = await loadedFormatter.format(
-              lintResults.filter((item) => item.warningCount > 0),
-            );
-            console.log('');
-            if (emitWarningAsError) {
-              this.error(formatResult);
-            } else {
-              this.warn(formatResult);
-            }
-          }
-        })
-        .catch((error) => {
-          console.log('');
-          this.error(`${error?.message ?? error}`);
-        });
+      await lintFiles(file);
 
       return null;
     },
