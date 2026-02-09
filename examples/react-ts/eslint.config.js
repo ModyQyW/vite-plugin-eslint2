4f53f722b542f36283d8d4558adcd368adc7138b
Panpan <wurui.dev@gmail.com>
2026-02-09 14:30:05 +0800

    feat: support eslint v10, bump example

--- examples/react-ts/eslint.config.js
+++ examples/react-ts/eslint.config.js
@@ -1,28 +1,15 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import eslintReact from "@eslint-react/eslint-plugin";
+import { defineConfig } from "eslint/config";
+import reactHooks from "eslint-plugin-react-hooks";
+import { reactRefresh } from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-)
+export default defineConfig(
+  { ignores: ["dist"] },
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  eslintReact.configs["recommended-typescript"],
+  reactHooks.configs.flat.recommended,
+  reactRefresh.configs.vite(),
+);
